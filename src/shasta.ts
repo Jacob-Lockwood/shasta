3f3aba3c9eb90d35f81ff6f904c6445bb48ee5c9
Jacob <jacobulysseslockwood@gmail.com>
2023-06-03 21:33:42 -0400

    Read pre-built lib

--- src/shasta.ts
+++ src/shasta.ts
@@ -4,12 +4,12 @@ import { compile } from "./compiler/compile";
 
 const file = readFileSync(stdin.fd, "utf8");
 const lib = readFileSync(
-  new URL("../src/lib/standard.shasta", import.meta.url),
+  new URL("../dist/lib/standard.js", import.meta.url),
   "utf8"
 );
 
 try {
-  const compiled = compile(lib + file);
+  const compiled = lib + "\n// END LIB\n" + compile(file);
   console.log(compiled);
 } catch (e) {
   console.error("Panic: Kablooey!", e);
